61c11b9fecb5bb9df89b60e739a99a90e7ac540e
Mike Thvedt <mike.thvedt@gmail.com>
2016-10-30 15:31:57 -0400

    More SpinFutures

--- rust/lib/store/src/tdfuture/spin.rs
+++ rust/lib/store/src/tdfuture/spin.rs
@@ -9,6 +9,73 @@ pub enum SpinResult<S: SpinLambda> {
 	Spin(SpinFuture<S>),
 }
 
+impl<S: SpinLambda> SpinResult<S> {
+	pub fn ok(item: S::Item) -> Self {
+		SpinResult::Ok(item)
+	}
+
+	pub fn err(err: S::Error) -> Self {
+		SpinResult::Err(err)
+	}
+
+	/// Combines a future with a SpinLambda to form a SpinFuture wrapped in a SpinResult.
+	pub fn chain(in_future: S::BlockingFuture, continuation: S) -> Self {
+		SpinResult::Spin(SpinFuture::chain(in_future, continuation))
+	}
+
+	/// Turns this SpinReslt into a Future, so it can be combined with future combinators.
+	/// Polling the resultant Future yields either the contained Item, the contained Err,
+	/// or polls the contained Future.
+	pub fn to_future(self) -> SpinResultFuture<S> {
+		match self {
+			SpinResult::Ok(item) => SpinResultFuture::Ok(item),
+			SpinResult::Err(err) => SpinResultFuture::Err(err),
+			SpinResult::Spin(f) => SpinResultFuture::Spin(f),
+		}
+	}
+}
+
+/// A future version of SpinResult. See `SpinResult::to_future(self)`.
+pub enum SpinResultFuture<S: SpinLambda> {
+	Ok(S::Item),
+	Err(S::Error),
+	Spin(SpinFuture<S>),
+	// The wrinkle is we need a way to safely move the Ok and Err states.
+	Consumed,
+}
+
+impl<S: SpinLambda> Future for SpinResultFuture<S> {
+	type Item = S::Item;
+	type Error = S::Error;
+
+	fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+		let mut oldself = unsafe { mem::uninitialized() };
+		let mut newself;
+		let r;
+		mem::swap(self, &mut oldself);
+
+		match oldself {
+			SpinResultFuture::Ok(item) => {
+				newself = SpinResultFuture::Consumed;
+				r = Ok(Async::Ready(item));
+			},
+			SpinResultFuture::Err(err) => {
+				newself = SpinResultFuture::Consumed;
+				r = Err(err);
+			}
+			SpinResultFuture::Spin(mut f) => {
+				r = f.poll();
+				newself = SpinResultFuture::Spin(f);
+			}
+			SpinResultFuture::Consumed => panic!("Cannot poll a complete future twice"),
+		}
+
+		mem::swap(self, &mut newself);
+		mem::forget(newself);
+		r
+	}
+}
+
 /// A closure that can return a SpinResult. In other words, it can return Ok(Self::Item),
 /// Error(Self::Error), or return a Future which might be blocked. (It doesn't have to be blocked.)
 /// In conjunction with SpinFuture, this can be used to represent a directly recursive Future
@@ -28,11 +95,11 @@ pub enum SpinResult<S: SpinLambda> {
 /// The returned future then replaces its internal copy. In this way an indefinite series of futures
 /// is represented without allocating or changing internal states.
 ///
-/// # Type theory note: Y not use the Y-combinator (or some other fixed-point combinator)?
+/// # CS note: Y not use the Y-combinator (or some other fixed-point combinator)?
 ///
 /// The (SpinLambda, SpinFuture) mechanism allows us to define a recursive Future without
-/// complicated types or fixed-point combinator gymnastics. The drawback is implementations of SpinFuture
-/// must go through SpinLambda, which is a little more verbose than a closure+combinator combo.
+/// complicated types or fixed-point combinator gymnastics, which are difficult to pull off (and understand)
+/// elegantly in Rust. At least, that was what I found.
 pub trait SpinLambda: Sized {
 	/// The blocked future type for this SpinLambda.
 	type BlockingFuture: Future<Error = Self::Error>;
@@ -50,7 +117,7 @@ pub struct SpinFuture<S: SpinLambda> {
 
 impl<S: SpinLambda> SpinFuture<S> {
 	/// Combines a future with a SpinLambda to form a SpinFuture.
-	pub fn chain(in_future: S::BlockingFuture, continuation: S) -> SpinFuture<S> {
+	pub fn chain(in_future: S::BlockingFuture, continuation: S) -> Self {
 		SpinFuture {
 			in_future: in_future,
 			spin_lambda: continuation,
@@ -64,7 +131,6 @@ impl<S: SpinLambda> Future for SpinFuture<S> {
 
 	fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
 		// Let's try spinning! That's a good trick!
-		// We 'spin' in_future and spin_lambda until we get something that blocks.
 		loop {
 			let mut newself = match self.in_future.poll() {
 				Ok(Async::NotReady) => return Ok(Async::NotReady),
@@ -83,51 +149,3 @@ impl<S: SpinLambda> Future for SpinFuture<S> {
 		}
 	}
 }
-
-// /// Takes a future that can return an item or another copy of itself,
-// /// and turns it into a future which just returns the item.
-// /// This can be used to wrap an indefinite loop of futures into one future.
-// ///
-// pub fn spin_future<F, Item>(future: F) -> SpinFuture<F, Item> where F: Future<Item = Result<Item, F>> {
-// 	SpinFuture {
-// 		inner: future,
-// 	}
-// }
-
-// /// Helper to turn a function into a spin_future.
-// ///
-// /// This turns a recursive future-based function and folds it into a single future with bounded internal state.
-// /// Useful for turning indefinite recursion into an O(1)-sized future.
-// ///
-// /// # Examples
-// pub fn spin_future<F, Lambda, EndItem>(start_item: &Item, lambda: L) -> SpinResult<Item, F> where
-// F: Future<Item = Result<Item, F>>,
-// L: Fn(&Item) -> SpinResult<EndItem, F>,
-// {
-// 	match lambda(item) {
-// 		Ok(end_item) => Ok(end_item),
-// 		Err(err) => Err(err),
-// 		Wait(f) => Wait(f)
-// 	}
-// }
-
-// // pub fn spinify<F, R>(lambda: L) -> SpinFuture<F, R> where
-// // L: FnOnce() -> FutureResult<F, R>,
-
-// impl<F, EndItem> Future for SpinFuture<F, EndItem> where F: Future<Item = Result<EndItem, F>> {
-//     type Item = EndItem;
-//     type Error = F::Error;
-
-//     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-//         match self.inner.poll() {
-// 	        Ok(Async::Ready(Ok(r))) => Ok(Async::Ready(r)),
-// 	        Ok(Async::Ready(Err(new_inner))) => {
-// 	            // The point of a spin future--replace self with the next one in the chain
-// 	            self.inner = new_inner;
-// 	            Ok(Async::NotReady)
-// 	        }
-// 	        Ok(Async::NotReady) => Ok(Async::NotReady),
-// 	        Err(err) => Err(err),
-//         }
-//     }
-// }
